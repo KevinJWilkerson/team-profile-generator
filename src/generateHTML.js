4d01aef0d50ae30963716405f90abb51a44bcb3a
Kevin <kevin.j.wilkerson@gmail.com>
2022-02-23 11:27:26 -0600

    added html constructor function to house employee html details

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,5 +1,5 @@
 // TODO: Create a function to generate HTML for Team Page
-const generateHTML = (employeeData) => {
+const generateEmployeeHTML = (employeeData) => {
   if (!employeeData) {
     return "";
   }
@@ -30,4 +30,40 @@ const generateHTML = (employeeData) => {
             `;
     }
   );
+};
+
+module.exports = (teamData) => {
+  console.log("Team Data: " + typeof teamData);
+  return `
+        <!DOCTYPE html>
+        <html lang="en">
+        
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Team Summary</title>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+            <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+            <link rel="stylesheet" href="style.css">
+        </head>
+            
+        <body>
+            <header>
+            <div class="container flex-row justify-space-between align-center py-3">
+                <h1 class="page-title text-secondary bg-dark py-2 px-3"></h1>
+                <nav class="flex-row">
+                
+                </nav>
+            </div>
+            </header>
+            <main class="container my-5">
+                ${generateEmployeeHTML(teamData)}
+            </main>
+            <footer class="container text-center py-3">
+            <h3 class="text-dark">&copy;2020 by </h3>
+            </footer>
+        </body>
+        </html>
+    `;
 };
