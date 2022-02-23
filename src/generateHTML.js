cbf7eb95cd43b0b3fd968cb387b7078151e34246
Kevin <kevin.j.wilkerson@gmail.com>
2022-02-23 14:19:35 -0600

    added email and github link functionality in html

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -12,7 +12,7 @@ const generateEmployeeHTML = (employeeData) => {
       if (officeNumber) {
         extended = `<h2>Office Number: ${officeNumber}</h3>`;
       } else if (github) {
-        extended = `<h2>Github: ${github}</h3>`;
+        extended = `<h2>Github: <a href="github.com/${github}" target="_blank">${github}</a></h3>`;
       } else if (school) {
         extended = `<h2>School: ${school}</h3>`;
       }
@@ -24,7 +24,7 @@ const generateEmployeeHTML = (employeeData) => {
                         <h2>ID: ${id}</h2>
                     </div>
                     <div class="card-body" id="employee-info">
-                        <h2>Email: ${email}</h2>
+                        <h2>Email: <a href="mailto:${email}">${email}</a></h2>
                         ${extended}
                 </section>
             `;
@@ -32,41 +32,6 @@ const generateEmployeeHTML = (employeeData) => {
   );
 };
 
-<<<<<<< HEAD
-module.exports = (teamData) => {
-  console.log("Team Data: " + typeof teamData);
-  return `
-        <!DOCTYPE html>
-        <html lang="en">
-        
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Team Summary</title>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-            <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
-            <link rel="stylesheet" href="style.css">
-        </head>
-            
-        <body>
-            <header>
-            <div class="container flex-row justify-space-between align-center py-3">
-                <h1 class="page-title text-secondary bg-dark py-2 px-3"></h1>
-                <nav class="flex-row">
-                
-                </nav>
-            </div>
-            </header>
-            <main class="container my-5">
-                ${generateEmployeeHTML(teamData)}
-            </main>
-            <footer class="container text-center py-3">
-            <h3 class="text-dark">&copy;2020 by </h3>
-            </footer>
-        </body>
-        </html>
-=======
 module.exports = (employeeData) => {
   console.log("teamData here: " + typeof employeeData);
   return `
@@ -100,6 +65,5 @@ module.exports = (employeeData) => {
         </footer>
     </body>
     </html>
->>>>>>> feature/inquirer
     `;
 };
